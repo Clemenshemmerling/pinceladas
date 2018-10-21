09dd609e42642113ac90ce48cdf0cdb2b9597946
Clemens Hemmerling <clemens@galileo.edu>
2018-10-21 17:55:25 -0600

    Correccion de php

--- contacto.php
+++ contacto.php
@@ -3,10 +3,10 @@
 	$email = $_POST['email'];
 	$telefono = $_POST['telefono'];
 	$mensaje = $_POST['descripcion'];
-	$bcc =  "Bcc: gerencia@creativeside.gt, desarrollo@creativeside.gt. "\r\n";
-	$cuerpomensaje = "Nombre: ".$nombre."\n"."E-Mail: ".$email."\n"."Telefono: ".$telefono."\n"."Mensaje: "."\n".$mensaje."\n\n\nURL: http://http://trotec.com.gt/";
-	mail("gerencia@creativeside.gt, desarrollo@creativeside.gt", "Landing TROTEC ",$cuerpomensaje, 'From:'. $email . "\r\n".$bcc);
-	header('refresh:2; url=index.php');
+	$bcc =  "Bcc: clemenshemmerling@gmail.com. "\r\n";
+	$cuerpomensaje = "Nombre: ".$nombre."\n"."E-Mail: ".$email."\n"."Telefono: ".$telefono."\n"."Mensaje: "."\n".$mensaje."\n\n\nURL: http://http://www.pinceladasnocturnas.com/";
+	mail("clemenshemmerling@gmail.com, clemenshemmerling@outlook.com", "Pinceladas Nocturnas ",$cuerpomensaje, 'From:'. $email . "\r\n".$bcc);
+	header('refresh:2; url=index.html');
 ?>
 
 
