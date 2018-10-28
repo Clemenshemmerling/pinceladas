fca1f801085a6d3e2bdde0c42db718a8cc715afa
Clemens Hemmerling <clemens@galileo.edu>
2018-10-28 17:45:23 -0600

    Cambios segeridos

--- contacto.php
+++ contacto.php
@@ -3,8 +3,8 @@
 	$email = $_POST['email'];
 	$telefono = $_POST['telefono'];
 	$mensaje = $_POST['descripcion'];
-	$bcc =  "Bcc: clemenshemmerling@gmail.com. "\r\n";
-	$cuerpomensaje = "Nombre: ".$nombre."\n"."E-Mail: ".$email."\n"."Telefono: ".$telefono."\n"."Mensaje: "."\n".$mensaje."\n\n\nURL: http://http://www.pinceladasnocturnas.com/";
+	$bcc =  "Bcc: sergio@pinceladasnocturnas.com";
+	$cuerpomensaje = "Nombre: ".$nombre."\n"."E-Mail: ".$email."\n"."Telefono: ".$telefono."\n"."Mensaje: "."\n".$mensaje."\n\n\nURL: http://pinceladasnocturnas.com/";
 	mail("clemenshemmerling@gmail.com, clemenshemmerling@outlook.com", "Pinceladas Nocturnas ",$cuerpomensaje, 'From:'. $email . "\r\n".$bcc);
 	header('refresh:2; url=index.html');
 ?>
